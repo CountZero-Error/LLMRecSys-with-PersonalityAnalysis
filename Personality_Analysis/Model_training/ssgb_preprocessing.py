879e66712dea042e58b2e0a425e0de837d38beb1
Cookie Lyu <cookielyu@gmail.com>
2025-01-04 18:09:14 +0800

    Semi-supervised

--- Personality_Analysis/Model_training/ssgb_preprocessing.py
+++ Personality_Analysis/Model_training/ssgb_preprocessing.py
@@ -1,14 +1,33 @@
-from sklearn.preprocessing import StandardScaler
-from datasets import load_dataset
-import networkx as nx
+from sentence_transformers import SentenceTransformer, util
+from nltk.corpus import wordnet
+from datasets import Dataset
+from tqdm import tqdm
 import pandas as pd
-import numpy as np
+import random
+import torch
+import nltk
+import os
 
 class ssgb_preprocessing:
     def __init__(self, df):
         self.df = df
 
+    def compute_embedding_consistency_loss(self, original_embeddings, augmented_embeddings):
+        # calculate Euclidean distance for consistency_loss
+        consistency_loss = torch.mean((original_embeddings - augmented_embeddings) ** 2)
+        return consistency_loss
+
+
+
+
+
+
+
+
+
 
     def run(self):
+
+
         lebeled_data_df = self.df[self.df['labels'].notna()]
         unlebeled_data_df = self.df[self.df['labels'].isna()]
--- Personality_Analysis/Model_training/ssgb_uda_embedding.py
+++
@@ -1,6 +0,0 @@
-from nltk.corpus import wordnet
-from datasets import Dataset
-from tqdm import tqdm
-import pandas as pd
-import random
-import nltk
